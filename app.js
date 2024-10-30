a5947f7c2c5788d718fd9c8337c9f6f0f08c05e0
pospak <pospk65@gmail.com>
2024-10-30 16:53:01 +0100

    základy frontendu

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{title:"Chyba!"});
 });
 
 // creates the tourdeapp table in the databace
