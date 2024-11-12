8bff85c2b79085e8efe29a6dd890a69ab5fa7321
Michal Pospíšil <pospk65@gmail.com>
2024-11-12 18:29:33 +0100

    tím pádem fe musí být pořešeno jinak

--- routes/api.js
+++ routes/api.js
@@ -589,14 +589,14 @@ router.get("/v1/games", (req, res) => {
           console.error("Chyba při dotazu do databáze:", err.message); // Zobraz chybovou zprávu
           res.status(500).json({ error: "Došlo k chybě při načítání dat." });
         } else {
-          
+          res.status(200).json(rows);
             res.render("games",{
               title: "Uložené hry",
               games: rows,
               board: board,
               formatDate
             })
-            res.status(200).json(rows);
+            
         }
     });
 });
@@ -627,12 +627,6 @@ router.get("/v1/games/:uuid", (req, res) => {
         });
       } else {
         // Pokud byla hra nalezena, renderuj konkrétní hru
-        res.render("game", {
-          title: game.name,
-          data: game,
-          board: JSON.parse(game.board),
-          formatDate,
-        });
         res.status(200).json({
           "uuid": game.uuid,
           "createdAt": game.createdAt,
@@ -642,6 +636,13 @@ router.get("/v1/games/:uuid", (req, res) => {
           "gameState": game.gameState,
           "board": JSON.parse(game.board) 
       });
+        res.render("game", {
+          title: game.name,
+          data: game,
+          board: JSON.parse(game.board),
+          formatDate,
+        });
+     
       }
     }
   });
